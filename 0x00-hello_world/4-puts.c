47c4d9376ab3648f878fd93c7bd5ba4f2c90a4d8
davidbodloza01 <dbodloza78@gmail.com>
2023-03-22 03:48:10 -0700

    c is fun .c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - an entry point to an app
+ * return 0 means success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
+
