cddf8ac2118bb7b195f11396c7c017ebfb708dd3
davidbodloza01 <dbodloza78@gmail.com>
2023-03-22 06:41:32 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - an entry point to an app
- * return 0 means success
+ *
+ * Return: 0 means success
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
-
